4f2342f0caf2b545b8d45e6a3f907b8700ab5863
Aman Tayal <omtayal9927@gmail.com>
2026-09-02 14:39:32 +0530

    new

---
+++ promisee.js
@@ -0,0 +1,22 @@
+function f1(){
+return new Promise((resolve, reject)=>{
+   setTimeout(()=>{
+console.log("task 1");
+resolve();
+}, 1000);
+});
+}
+function f2(){
+return new Promise((resolve, reject)=>{
+   setTimeout(()=>{
+console.log("task 2");
+resolve();
+}, 1000);
+});
+}
+f1().then(f2).then(()=>{
+    console.log("task 3");
+})
+.catch((err)=>{
+    console.log(err);
+})
